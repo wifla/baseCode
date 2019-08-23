c5edd24994722a7a6e149a61192411feeb81eb53
wifla <805436066@qq.com>
2019-08-23 15:01:53 +0800

    add calendar

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,18 +14,23 @@ Page({
       countTime: '',
       windowWidth: null,
       length: null
-    }
-    
+    },
+    //存放day的样式
+    daysArr:[]
   },
 
   onLoad: function () {
-    let data = {};
+    this.initNotice();
+    this.initCalendar();
+  },
+  //初始化notice
+  initNotice: function(){
     let length = this.data.noticeConfig.notice.length * this.data.noticeConfig.size; //文字长度
     let windowWidth = wx.getSystemInfoSync().windowWidth; // 屏幕宽度
     let config = this.data.noticeConfig;
     config.length = length;
     config.windowWidth = windowWidth;
-    this.setData({ 
+    this.setData({
       noticeConfig: config
     });
     let noticeConfig = this.data.noticeConfig;
@@ -35,6 +40,7 @@ Page({
     });
     this.startNotice();
   },
+  //通告栏执行函数
   startNotice: function(){
     let that = this;
     that.data.noticeConfig.countTime = setInterval(function () {
@@ -53,5 +59,27 @@ Page({
         that.startNotice();
       }
     }, that.data.noticeConfig.interval);
+  },
+  //初始化calendar
+  initCalendar: function(){
+    let daysArr = [];
+    let nowDate = new Date();
+    let year = new Date().getFullYear();
+    let month = new Date().getMonth();
+    let nowDay = new Date().getDate();
+    let daysNum = new Date(year,month+1,0).getDate();
+    console.log(daysNum,month);
+    for (let i = 1; i <= daysNum; i++) {
+      daysArr.push({
+        month: 'current', day: i, color: 'rgb(18, 150, 219)'
+      });
+    }
+    //当前日期
+    daysArr.push(
+      { month: 'current', day: nowDay, color: 'white', background: 'rgba(18, 150, 219,0.5)' },
+    ); 
+    this.setData({
+      daysArr: daysArr
+    })
   }
 })
